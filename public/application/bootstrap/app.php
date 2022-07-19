319f675509923f5c06808ce2cfed884c0f161b0e
parasek <slawek@radomski.net.pl>
2022-07-19 16:03:38 +0200

    Load custom classes from src directory

--- public/application/bootstrap/app.php
+++ public/application/bootstrap/app.php
@@ -64,13 +64,16 @@
  * ----------------------------------------------------------------------------
  */
 
-/*
- * Php error reporting level
- * Default Concrete5 values: E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED (22519)
- * Calculator: https://maximivanov.github.io/php-error-reporting-calculator/
- * Comment line below to use default values
- */
+// PHP error reporting level
+// Default Concrete value: E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED (22519)
+// Calculator: https://maximivanov.github.io/php-error-reporting-calculator/
+// Comment line below to go back to default values
 error_reporting(E_ALL);
+
+// Autoload custom classes from "public/application/src" directory
+$strictLoader = new \Concrete\Core\Foundation\Psr4ClassLoader();
+$strictLoader->addPrefix('Application', DIR_APPLICATION . '/' . DIRNAME_CLASSES);
+$strictLoader->register();
 
 // Register custom urls
 $this->app->make('Concrete\Core\Routing\RouterInterface')->registerMultiple([
