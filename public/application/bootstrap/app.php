53cd0fa465ea7e9c0be228ffbd540f383f8e341b
parasek <slawek@radomski.net.pl>
2022-10-20 14:33:47 +0200

    Disable strict reporting

--- public/application/bootstrap/app.php
+++ public/application/bootstrap/app.php
@@ -68,7 +68,7 @@
 // Default Concrete value: E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED (22519)
 // Calculator: https://maximivanov.github.io/php-error-reporting-calculator/
 // Comment line below to go back to default values
-error_reporting(E_ALL);
+//error_reporting(E_ALL);
 
 // Autoload custom classes from "public/application/src" directory
 $strictLoader = new \Concrete\Core\Foundation\Psr4ClassLoader();
