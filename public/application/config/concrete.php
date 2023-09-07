9f399eb3a8730d36b10c292a4ad602fdd01d4961
parasek <slawek@radomski.net.pl>
2023-09-07 15:47:17 +0200

    Add caching options to concrete config

--- public/application/config/concrete.php
+++ public/application/config/concrete.php
@@ -1,16 +1,35 @@
 <?php
 
 return [
-    'updates' => [
-        // Skip the automatic check of new Concrete versions availability
-        'skip_core' => true,
-    ],
     'debug' => [
+        'display_errors' => true, // (true | false)
+        'detail' => 'debug', // (message | debug)
         'hide_keys' => [
             // Hide database password and hostname in whoops output if supported
             '_ENV' => ['DB_PASSWORD', 'DB_HOSTNAME'],
             '_SERVER' => ['DB_PASSWORD', 'DB_HOSTNAME'],
         ],
+    ],
+    'cache' => [
+        'enabled' => false, // Enabled (true | false)
+        'overrides' => false, // Overrides Cache (true | false)
+        'blocks' => false, // Block Output Cache (true | false)
+        'assets' => false, // CSS and JavaScript Post-Processing (true | false)
+        'theme_css' => false, // Theme CSS Cache (true | false)
+        'pages' => false, // Full Page Caching (false | 'blocks' | 'all')
+        'lifetime' => 21600, // Lifetime in seconds
+        'full_page_lifetime' => 'default', // How long to cache full page
+        'full_page_lifetime_value' => null, // Custom lifetime value, only used if concrete.cache.full_page_lifetime is 'custom'
+        'doctrine_dev_mode' => false, // Use Doctrine development mode
+    ],
+    'theme' => [
+        // Those are only used when using LESS files
+        'compress_preprocessor_output' => true, // Compress LESS Output
+        'generate_less_sourcemap' => true, // Enable source maps in generated CSS files
+    ],
+    'updates' => [
+        // Skip the automatic check of new Concrete versions availability
+        'skip_core' => true,
     ],
     'email' => [
         'enabled' => true,
